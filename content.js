36da13a7c372f62abc5f00ad0ac1713b0524c448
Brandon Fonseca <brandonfonseca5@gmail.com>
2019-08-02 12:04:29 -0400

    Injecting the score onto the screen

--- content.js
+++ content.js
@@ -1,25 +1,25 @@
 $(document).click(function() {
-    const title_element = $('.title')[1];
+    const title_element_array = $('.title');
+    const title_element = title_element_array[title_element_array.length - 1];
     const title = title_element.getElementsByTagName('img')[0].alt;
     console.log(title);
-    getRatings(title);
-
+    injectData(getRatings(title));
 });
 
 function getRatings(title) {
-    $.get("https://www.omdbapi.com/?apikey=***REMOVED***&t="+title, function(data){
-        console.log(data);
-        const imdb_rating = data.imdbRating;
-        const metescore = data.Metascore;
-        const rotten_tomatoes_rating = getRottenTomatoScore(data);
-        ratings = {
-            "IMDB": imdb_rating,
-            "Metascore": metescore,
-            "Rotten Tomatoes": rotten_tomatoes_rating
-        };
-        console.log(ratings);
-    });
-
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://www.omdbapi.com/?apikey=***REMOVED***&t="+title, false );
+    xmlHttp.send( null );
+    data = JSON.parse(xmlHttp.response);
+    const imdb_rating = data.imdbRating;
+    const metescore = data.Metascore;
+    const rotten_tomatoes_rating = getRottenTomatoScore(data);
+    ratings = {
+        "IMDB": imdb_rating,
+        "Metascore": metescore,
+        "Rotten Tomatoes": rotten_tomatoes_rating
+    };
+    return ratings
 }
 
 function getRottenTomatoScore(data) {
@@ -30,4 +30,17 @@ function getRottenTomatoScore(data) {
         }
     }
     return "N/A";
+}
+
+function injectData(ratings){
+    node_array = $('.video-meta ');
+    node = node_array[node_array.length - 1];
+    var imdb_span = document.createElement("SPAN");
+    var imdb_score = document.createTextNode("IMDB: " + ratings["IMDB"].toString());
+    var metascore = document.createTextNode("Metascore: " + ratings["Metascore"].toString());
+    var rotten_tomatoes = document.createTextNode("Rotten Tomatoes: " + ratings["Rotten Tomatoes"].toString());
+    imdb_span.appendChild(imdb_score);
+    imdb_span.appendChild(metascore);
+    imdb_span.appendChild(rotten_tomatoes);
+    node.appendChild(imdb_span);
 }
